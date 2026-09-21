4ec2cf1de595403f6d4ad5b08d3dc40087dc047a
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2026-09-21 14:57:02 +0800

    test: cover Bluetooth and notification permission states

---
+++ app/src/test/java/me/dizzykitty3/androidtoolkitty/utils/PermissionUtilsTest.kt
@@ -0,0 +1,44 @@
+package me.dizzykitty3.androidtoolkitty.utils
+
+import android.app.Application
+import me.dizzykitty3.androidtoolkitty.BT_CONNECT
+import me.dizzykitty3.androidtoolkitty.POST_NOTIFICATIONS
+import me.dizzykitty3.androidtoolkitty.utils.PermissionUtils.noBluetoothPermission
+import me.dizzykitty3.androidtoolkitty.utils.PermissionUtils.noNotificationPermission
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.Shadows.shadowOf
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
+class PermissionUtilsTest {
+
+    @Test
+    fun notificationPermission_reflectsGrantedAndDeniedStates() {
+        val application = application()
+
+        shadowOf(application).denyPermissions(POST_NOTIFICATIONS)
+        assertTrue(application.noNotificationPermission())
+
+        shadowOf(application).grantPermissions(POST_NOTIFICATIONS)
+        assertFalse(application.noNotificationPermission())
+    }
+
+    @Test
+    fun bluetoothPermission_reflectsGrantedAndDeniedStatesOnAndroid12AndLater() {
+        val application = application()
+
+        shadowOf(application).denyPermissions(BT_CONNECT)
+        assertTrue(application.noBluetoothPermission())
+
+        shadowOf(application).grantPermissions(BT_CONNECT)
+        assertFalse(application.noBluetoothPermission())
+    }
+
+    private fun application(): Application = RuntimeEnvironment.getApplication()
+}
