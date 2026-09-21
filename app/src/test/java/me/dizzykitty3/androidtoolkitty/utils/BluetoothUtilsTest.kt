2540c9c4ff5c40384c109558330dd043758fea21
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2026-09-21 17:01:33 +0800

    test: cover Bluetooth adapter access with permission

--- app/src/test/java/me/dizzykitty3/androidtoolkitty/utils/BluetoothUtilsTest.kt
+++ app/src/test/java/me/dizzykitty3/androidtoolkitty/utils/BluetoothUtilsTest.kt
@@ -1,12 +1,15 @@
 package me.dizzykitty3.androidtoolkitty.utils
 
 import android.content.Context
+import me.dizzykitty3.androidtoolkitty.BT_CONNECT
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
+import org.robolectric.Shadows.shadowOf
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
@@ -19,5 +22,13 @@ class BluetoothUtilsTest {
 
         assertFalse(context.isHeadsetConnected())
         assertTrue(context.headsetNotConnected())
+    }
+
+    @Test
+    fun bluetoothAdapter_isAvailableWhenBluetoothPermissionIsGranted() {
+        val context: Context = RuntimeEnvironment.getApplication()
+        shadowOf(RuntimeEnvironment.getApplication()).grantPermissions(BT_CONNECT)
+
+        assertNotNull(context.bluetoothAdapter())
     }
 }
